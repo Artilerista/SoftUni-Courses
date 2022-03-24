fdf2a32aae542d6fd433fb70719eb9f991f6ebef
Artilerista <Aleksandar_todorov98@abv.bg>
2022-03-24 10:59:18 +0200

    Added my work for last half a year in SoftUni

--- Programming Basics/01.First exercise/Inches to Centimeters.js
+++
@@ -1,6 +0,0 @@
-function incechesToCentimeters(input){
-    let a = Number(input[0]);
-    let b = a*2.54;
-    console.log(b);
-    }
-    incechesToCentimeters(['5'])
--- Programming Basics/01.First exercise/script.js
+++
@@ -1,4 +0,0 @@
-function demo(){
-console.log("Hello SoftUni");
-}
-demo();
