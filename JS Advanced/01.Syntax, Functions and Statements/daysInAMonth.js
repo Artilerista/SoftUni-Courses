d773592e51b79d415d9f0c1d49c450991abd1627
Artilerista <aleksandar_todorov98@abv.bg>
2022-05-21 14:15:06 +0300

    Add exercise from Advanced

---
+++ JS Advanced/01.Syntax, Functions and Statements/daysInAMonth.js
@@ -0,0 +1,32 @@
+function daysInAMonth (month,year) {
+    if (month == '1'){
+        console.log('31')
+    } else if (month == '2') {
+        if(year % 4 === 0) {
+            console.log('29');
+        } else {
+            console.log('28');
+        }
+    } else if (month == '3'){
+        console.log('31');
+    } else if (month == '4'){
+        console.log('30');
+    } else if (month == '5'){
+        console.log('31');
+    } else if (month == '6'){
+        console.log('30');
+    } else if (month == '7'){
+        console.log('31');
+    } else if (month == '8'){
+        console.log('31');
+    } else if (month == '9'){
+        console.log('30');
+    } else if (month == '10'){
+        console.log('31');
+    } else if (month == '11'){
+        console.log('30');
+    } else if (month == '12'){
+        console.log('31');
+    }
+}
+daysInAMonth(1, 2012)
