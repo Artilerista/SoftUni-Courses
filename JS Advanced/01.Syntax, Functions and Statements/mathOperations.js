b9529dbe9a83fd18b656645e82efc4b45f655577
Artilerista <aleksandar_todorov98@abv.bg>
2022-05-21 14:15:19 +0300

    Add exercise from Advanced

---
+++ JS Advanced/01.Syntax, Functions and Statements/mathOperations.js
@@ -0,0 +1,16 @@
+function mathOperations(n1, n2, operation) {
+    if (operation === '+'){
+        console.log(n1 + n2);
+    } else if (operation === '-'){
+        console.log(n1 - n2);
+    } else if (operation === '*'){
+        console.log(n1 * n2);
+    } else if (operation === '**'){
+        console.log(n1 ** n2);
+    } else if (operation === '/'){
+        console.log(n1 / n2);
+    } else if (operation === '%'){
+        console.log(n1 % n2);
+    }
+}
+mathOperations(5, 6, '+')
