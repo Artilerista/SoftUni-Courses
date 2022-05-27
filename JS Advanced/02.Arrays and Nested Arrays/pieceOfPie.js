83d3d4507de00e1af0cd5171c3e51f4a1203b95f
Artilerista <aleksandar_todorov98@abv.bg>
2022-05-27 21:50:08 +0300

    Add exercise from Advanced : Arrays and Nested Arrays

---
+++ JS Advanced/02.Arrays and Nested Arrays/pieceOfPie.js
@@ -0,0 +1,14 @@
+function pieceOfPie(arr, start, end) {
+    let startIndex = arr.indexOf(start);
+    let endIndex = arr.indexOf(end);
+    let newArr = arr.slice(startIndex, endIndex + 1);
+    return newArr;
+}
+pieceOfPie(['Pumpkin Pie',
+    'Key Lime Pie',
+    'Cherry Pie',
+    'Lemon Meringue Pie',
+    'Sugar Cream Pie'],
+    'Key Lime Pie',
+    'Lemon Meringue Pie'
+)
