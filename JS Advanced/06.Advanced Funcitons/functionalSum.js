0a9e4e9b1256921f5b8af779cff9e1b56f901c20
Artilerista <aleksandar_todorov98@abv.bg>
2022-06-10 21:53:01 +0300

    Add exercise from Advanced - Advanced Functions

---
+++ JS Advanced/06.Advanced Funcitons/functionalSum.js
@@ -0,0 +1,11 @@
+function add(n) {
+   let inner = function (a) {
+        n += a;
+        return inner;
+    }
+    inner.toString = function () {
+        return n
+    };
+    return inner;
+}
+console.log(add(1)(6)(-3).toString())
