14c9c1b2b372af9414ae86e7eef9fb274d324798
Artilerista <aleksandar_todorov98@abv.bg>
2022-06-24 09:19:04 +0300

    Add exercise from Advanced Prototypes and Inheritance

---
+++ JS Advanced/09.Prototypes and Inheritance/computer.js
@@ -0,0 +1,47 @@
+function solution() {
+    class Keyboard {
+        constructor(manufacturer, responseTime) {
+            this.manufacturer = manufacturer;
+            this.responseTime = responseTime;
+        }
+    }
+    class Monitor {
+        constructor(manufacturer, width, height) {
+            this.manufacturer = manufacturer;
+            this.width = width;
+            this.height = height;
+        }
+    }
+    class Battery {
+        constructor(manufacturer, expectedLife) {
+            this.manufacturer = manufacturer;
+            this.expectedLife = expectedLife;
+        }
+    }
+    class Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace){
+            if(new.target = Computer) {
+                throw new Error();
+            }
+            this.manufacturer = manufacturer;
+            this.processorSpeed = processorSpeed;
+            this.ram = ram;
+            this.hardDiskSpace = hardDiskSpace;
+        }
+    }
+    class Laptop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace , weight, color, )
+    }
+}
+let classes = createComputerHierarchy();
+let Computer = classes.Computer;
+let Laptop = classes.Laptop;
+let Desktop = classes.Desktop;
+let Monitor = classes.Monitor;
+let Battery = classes.Battery;
+let Keyboard = classes.Keyboard;
+
+let battery = new Battery('Energy', 3);
+console.log(battery);
+let laptop = new Laptop("Hewlett Packard", 2.4, 4, 0.5, 3.12, "Silver", battery);
+console.log(laptop);
